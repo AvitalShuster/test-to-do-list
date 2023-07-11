842f8fbe0a0e52e8a86c385d4f6b59b7d3d1a801
Avital Shusterman <avitalsh@amsalem.com>
2023-07-11 17:15:30 +0300

    Added alerts and changed the web page visibility

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { cx } from "@emotion/css";
+import Tooltip from "@mui/material/Tooltip";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import * as React from "react";
 import { css } from "@emotion/css";
+import CloseIcon from "@mui/icons-material/Close";
+import Collapse from "@mui/material/Collapse";
 import {
   Typography,
   Box,
   Checkbox,
   TextField,
-  ButtonGroup,
   Button,
   List,
   ListItemButton,
@@ -16,38 +21,6 @@ import {
   ListItemSecondaryAction,
   IconButton,
 } from "@mui/material";
-
-const containerStyle = css`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  gap: 30px;
-  .css-o2w69a-MuiTypography-root {
-    color: #29b6f6;
-    font-size: 50px;
-    font-weight: bold;
-    margin-top: 30px;
-  }
-`;
-
-const listContainerStyle = css`
-  width: 100%;
-  max-height: 450px;
-  overflow-x: hidden;
-  overflow-y: auto;
-`;
-
-const completedTaskStyle = css`
-  text-decoration: line-through solid 2px;
-  color: black;
-`;
-
-const FilterByTaskStateOption = {
-  ALL: "all",
-  TODO: "toDo",
-  COMPLETED: "completed",
-};
 
 const ToDoList = () => {
   const storedTasks = localStorage.getItem("tasks");
@@ -60,9 +33,12 @@ const ToDoList = () => {
   const completedTasks = tasks.filter((task) => task.isCompleted);
   const remainingTasks = tasks.filter((task) => !task.isCompleted);
 
+  const [showEmptyStringAlert, setShowEmptyStringAlert] = useState(false);
+  const [showTaskExistsAlert, setShowTaskExistsAlert] = useState(false);
+
   const addTask = () => {
     if (inputValue.trim() === "") {
-      alert("Trying to add an empty string!");
+      setShowEmptyStringAlert(true);
       return;
     }
 
@@ -71,7 +47,7 @@ const ToDoList = () => {
     );
 
     if (isTaskExists) {
-      alert("Task already exists!");
+      setShowTaskExistsAlert(true);
       return;
     }
 
@@ -85,6 +61,9 @@ const ToDoList = () => {
     setTasks(updatedTasks);
     setInputValue("");
     localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+
+    setShowEmptyStringAlert(false);
+    setShowTaskExistsAlert(false);
   };
 
   const deleteTask = (taskId) => {
@@ -110,33 +89,76 @@ const ToDoList = () => {
 
   return (
     <div className={containerStyle}>
-      <Typography variant="h1">To Do List</Typography>
-      <Box>
-        <TextField
-          id="outlined-basic"
-          label="Add a new task here"
-          variant="outlined"
-          size="small"
-          input
-          value={inputValue}
-          onChange={(event) => setInputValue(event.target.value)}
-        />
-        <Button variant="outlined" size="large" onClick={addTask}>
-          Add Task
-        </Button>
+      <Collapse in={showEmptyStringAlert}>
+        <Alert
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setShowEmptyStringAlert(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          severity="error"
+        >
+          <AlertTitle>Error</AlertTitle>Trying to add an empty string!
+        </Alert>
+      </Collapse>
 
-        <Box display="flex" flexDirection="column" alignItems="center">
-          <ButtonGroup
+      <Collapse in={showTaskExistsAlert}>
+        <Alert
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setShowTaskExistsAlert(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          severity="error"
+        >
+          <AlertTitle>Error</AlertTitle>Task already exists!
+        </Alert>
+      </Collapse>
+      <Typography variant="h1">To Do List</Typography>
+      <Box className={boxStyle}>
+        <div>
+          <TextField
+            id="outlined-basic"
+            label="Add a new task here"
             variant="outlined"
+            size="small"
+            input
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+          />
+          <Button variant="outlined" size="large" onClick={addTask}>
+            Add Task
+          </Button>
+        </div>
+
+        <Box>
+          <div
             size="medium"
             aria-label="outlined primary button group"
+            className={buttonGroupStyle}
           >
             <Button
+              variant="contained"
               onClick={() => setTaskStateFilter(FilterByTaskStateOption.ALL)}
             >
               All Tasks
             </Button>
             <Button
+              variant="contained"
               onClick={() =>
                 setTaskStateFilter(FilterByTaskStateOption.COMPLETED)
               }
@@ -144,21 +166,17 @@ const ToDoList = () => {
               Completed
             </Button>
             <Button
+              variant="contained"
               onClick={() => setTaskStateFilter(FilterByTaskStateOption.TODO)}
             >
               To Do
             </Button>
-          </ButtonGroup>
+          </div>
         </Box>
 
         <List className={listContainerStyle}>
           {filterTasks().map((task) => (
-            <ListItemButton
-              component="li"
-              href="#simple-list"
-              key={task.id}
-              disableGutters
-            >
+            <ListItemButton component="li" href="#simple-list" key={task.id}>
               <ListItemIcon>
                 <Checkbox
                   checked={task.isCompleted}
@@ -172,13 +190,15 @@ const ToDoList = () => {
                 }}
               />
               <ListItemSecondaryAction>
-                <IconButton
-                  edge="end"
-                  aria-label="delete"
-                  onClick={() => deleteTask(task.id)}
-                >
-                  <DeleteIcon />
-                </IconButton>
+                <Tooltip title="Delete">
+                  <IconButton
+                    edge="end"
+                    aria-label="delete"
+                    onClick={() => deleteTask(task.id)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </Tooltip>
               </ListItemSecondaryAction>
             </ListItemButton>
           ))}
@@ -189,7 +209,8 @@ const ToDoList = () => {
         display="flex"
         flexDirection="column"
         justifyContent="center"
-        alignItems="center"
+        alignItems="start"
+        gap="10px"
       >
         <Typography variant="body1" fontWeight="bold">
           Total Tasks: {tasks.length}
@@ -206,3 +227,58 @@ const ToDoList = () => {
 };
 
 export default ToDoList;
+
+const containerStyle = css`
+  display: flex;
+  justify-content: start;
+  align-items: start;
+  flex-direction: column;
+  gap: 30px;
+  padding: 20px;
+
+  .css-1qedkg0 {
+    width: 99%;
+  }
+
+  .css-o2w69a-MuiTypography-root {
+    color: #29b6f6;
+    font-size: 50px;
+    font-weight: bold;
+  }
+`;
+
+const listContainerStyle = css`
+  max-height: 450px;
+  overflow-y: auto;
+  background-color: #f5f5f5;
+  border-radius: 10px;
+  width: 100%;
+`;
+
+const completedTaskStyle = css`
+  text-decoration: line-through solid 2px;
+  color: black;
+`;
+
+const boxStyle = css`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+
+  > div {
+    display: flex;
+    gap: 30px;
+    align-items: center;
+  }
+`;
+
+const buttonGroupStyle = css`
+  display: flex;
+  gap: 20px;
+`;
+
+const FilterByTaskStateOption = {
+  ALL: "all",
+  TODO: "toDo",
+  COMPLETED: "completed",
+};
